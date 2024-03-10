386491406b784e64d78878c1207631f488b040ec
Sam <samuelgardner101@gmail.com>
2024-03-10 18:17:22 +0000

    Directory node information

--- src/distributed_hash_table/DHT.py
+++ src/distributed_hash_table/DHT.py
@@ -8,6 +8,8 @@ class NodeNotInNetworkException(Exception):
 
 
 class DHT:
+    DIRECTORY_NODES = {"192.168.1.49": b""}
+
     @staticmethod
     def get_static_public_key(address: Str) -> SecureBytes:
         cache = json.load(open("./_cache/dht_cache.json"))
@@ -26,3 +28,7 @@ class DHT:
         random_id = random.choices(cache, k=1)[0]
         random_id = base58.b58decode(random_id).decode()
         return random_id
+
+    @staticmethod
+    def get_random_directory_node() -> Str:
+        return random.choice(list(DHT.DIRECTORY_NODES.keys()))
