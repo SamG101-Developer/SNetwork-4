5ab1d36d554b631081ed754669b08432eac677a7
Sam <samuelgardner101@gmail.com>
2024-03-10 18:17:04 +0000

    Refactor + directory node commands

--- src/control_communications/ControlConnectionProtocol.py
+++ src/control_communications/ControlConnectionProtocol.py
@@ -1,20 +1,35 @@
 from enum import Enum
 
 
-class ControlConnectionProtocol(Enum):
-    CONN_REQ     = 0b0000  # Request a connection
-    CONN_ACC     = 0b0001  # Accept a connection request
-    CONN_REJ     = 0b0011  # Reject a connection request
-    CONN_CLS     = 0b0100  # Close a connection
-    CONN_ERR     = 0b0101  # Error in connection
-    CONN_FWD     = 0b0110  # Forward a connection command
-    CONN_EXT     = 0b0111  # Extend a connection
-    CONN_SEC     = 0b1000  # E2E connection is secured
-    CONN_EXT_ACC = 0b1001  # Acknowledge an extended connection
-    CONN_EXT_REJ = 0b1010  # Reject an extended connection
-    CONN_PKT_KEM = 0b1011  # Packet key: send pub key for KEM
-    CONN_PKT_KEY = 0b1100  # Packet key: send KEM-wrapped key
-    CONN_PKT_ACK = 0b1101  # Packet key: acknowledge key receipt
+# Base class for all connection protocols
+class ConnectionProtocol(Enum):
+    ...
 
 
-__all__ = ["ControlConnectionProtocol"]
+# Interacting with other nodes
+class ControlConnectionProtocol(ConnectionProtocol):
+    CONN_REQ     = 0b00000  # Request a connection
+    CONN_ACC     = 0b00001  # Accept a connection request
+    CONN_REJ     = 0b00011  # Reject a connection request
+    CONN_CLS     = 0b00100  # Close a connection
+    CONN_ERR     = 0b00101  # Error in connection
+    CONN_FWD     = 0b00110  # Forward a connection command
+    CONN_EXT     = 0b00111  # Extend a connection
+    CONN_SEC     = 0b01000  # E2E connection is secured
+    CONN_EXT_ACC = 0b01001  # Acknowledge an extended connection
+    CONN_EXT_REJ = 0b01010  # Reject an extended connection
+    CONN_PKT_KEM = 0b01011  # Packet key: send pub key for KEM
+    CONN_PKT_KEY = 0b01100  # Packet key: send KEM-wrapped key
+    CONN_PKT_ACK = 0b01101  # Packet key: acknowledge key receipt
+
+
+# Interacting with the directory nodes
+class DirectoryConnectionProtocol(ConnectionProtocol):
+    DIR_REG     = 0b01110  # Register a new node to the directory node + first certificate
+    DIR_CER_REQ = 0b01111  # Request a new time-updated certificate for a node from a directory node
+    DIR_CER     = 0b10000  # Certificate for a node from a directory node
+    DIR_LST_REQ = 0b10001  # Request a directory node for a list of nodes
+    DIR_LST_RES = 0b10010  # Response to a list request
+
+
+__all__ = ["ConnectionProtocol", "ControlConnectionProtocol", "DirectoryConnectionProtocol"]
