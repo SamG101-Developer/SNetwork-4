253cf1cd134e1b797b7c1844271a0a6caa6c8d07
Sam <samuelgardner101@gmail.com>
2024-03-27 21:07:55 +0000

    Debug

--- src/control_communications_2/ConnectionHub.py
+++ src/control_communications_2/ConnectionHub.py
@@ -116,10 +116,15 @@ def CreateSecureConnection(address: str) -> SecureSocket:
         message=my_ephemeral_public_key,
         their_id=DHT.get_id(address))
 
+    print(1)
+
     # Create the socket and send the connection request.
     conn = CreateUnsecureConnection(address)
+    print(2)
     request = ConnectionDataPackage(command=ConnectionProtocol.CON_CON_REQ, data=my_ephemeral_public_key_signed)
+    print(3)
     conn.send(request)
+    print(4)
 
     # Receive either a CON_CON_[ACC|REJ], or a DHT_CER_REQ.
     response = conn.recv()
